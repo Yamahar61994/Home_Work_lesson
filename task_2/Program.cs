fcc4e1b69d453fd403e28d274d80d883e127eb7d
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-08-31 02:00:17 +0300

    Неправельно работает код

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+
+Console.WriteLine("Введите число а= ");
+int a = Console.Read();
+
+Console.WriteLine("Введите число b= ");
+int b = Console.Read();
+
+if (a>b);
+{
+	Console.WriteLine($"Наибольшее=  {a}");
+	Console.WriteLine($"Наименьшее=  {b}");
+}
