a5855704b34e182e00b7777a99199f2c7db7c7ad
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-09-01 22:17:42 +0300

    Четвёртая готова!)

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -5,28 +5,18 @@
 */
 
 Console.WriteLine("Введите первое число ");
-int number1 = Console.ReadLine();
+ int number1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число ");
-int number2 = Console.ReadLine();
+int number2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите третье число ");
-int number3 = Console.ReadLine;
+int number3 = Convert.ToInt32(Console.ReadLine());
 
 int count = 0;
 int max =number1;
 
-if (number2>max);
-{
-	max = number2;
-	if (max>number3);
-	{
-		Console.WriteLine("Наибольше число из трёх = {max}");
-	}
-}
-else
-{
-	if (number3>max);
-	{
-		max = number3;
-	}
-}
-Console.WriteLine("Наибольше число из трёх = {max}");
+if (number2>max) max = number2;
+if (number3>max) max = number3;
+
+Console.WriteLine($"Наибольшее = {max}");
