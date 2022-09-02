3d17ae9359386d9014e36c424c37b0397bd1b0db
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-09-02 23:27:06 +0300

    13 задача решена

---
+++ task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число: ");
+string? chislo = Convert.ToString(Console.ReadLine());
+int leng = chislo.Length;
+
+
+if (leng>3) 
+	Console.WriteLine($"Третья цифра {chislo[2]}");
+else
+	Console.WriteLine($"Третьей цифры нет!");
