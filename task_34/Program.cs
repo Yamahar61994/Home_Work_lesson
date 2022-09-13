827c3d93258551f7026aa3cd72bbea2386fb58aa
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-09-13 22:20:16 +0300

    34 задача готова

---
+++ task_34/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arrayRand = new int[size];
+int count = 0;
+
+for (int i=0; i<arrayRand.Length ; i++)
+{
+	arrayRand[i] = new Random().Next(100,1000);
+	int ost = arrayRand[i] % 2;
+	if (ost == 0) count++;
+}
+Console.WriteLine($"[{String.Join(",\t", arrayRand)}]  --> {count}");
