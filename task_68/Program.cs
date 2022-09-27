026972ba0f0cdea3fe343bd953279533711bc304
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-09-27 22:49:57 +0300

    68 решена, только стек переполняется

---
+++ task_68/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+ Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+
+ */
+Console.WriteLine("Введите значение m: ");
+double  m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение n: ");
+double  n = Convert.ToDouble(Console.ReadLine());
+
+double Akerman(double m, double n)
+{
+    if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akerman(n - 1, 1);
+    else
+      return Akerman(n - 1, Akerman(n, m - 1));
+}
+Console.WriteLine(Akerman(m,n));
